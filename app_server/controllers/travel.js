aa7bcf9404f76980178ee0da12ede487115f22d2
Jeffrey Steele <jeffrey.steele1@snhu.edu>
2024-07-18 21:48:51 -0500

    Updated travel controller and view to use Handlebars loop for trips; added trips.json

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
-var path = require('path');
 
-// Correct path to the data folder in the main project directory
-var trips = JSON.parse(fs.readFileSync(path.join(__dirname, '../../data/trips.json'), 'utf8'));
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
